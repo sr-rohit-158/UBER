1af5230685861574c3d3593c79210f8f7627ca15
sr-rohit-158 <rohit.dhori@swiftrut.com>
2025-01-01 21:18:50 +0530

    initialize frontend with React, Vite, and Tailwind CSS setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React, { useContext } from 'react'
+import 'remixicon/fonts/remixicon.css'
+
+const App = () => {
+
+  return (
+    <div>
+      hi
+    </div>
+  )
+}
+
+export default App
