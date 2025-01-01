3d909c508f92aee6f0889172980f8b1c7338e217
sr-rohit-158 <rohit.dhori@swiftrut.com>
2025-01-01 21:59:21 +0530

    add user and captain authentication pages with routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,22 @@
 import React, { useContext } from 'react'
 import 'remixicon/fonts/remixicon.css'
+import UserLogin from './pages/UserLogin'
+import UserSignup from './pages/UserSignup'
+import { Route, Routes } from 'react-router-dom'
+import Captainlogin from './pages/Captainlogin'
+import CaptainSignup from './pages/CaptainSignup'
 
 const App = () => {
 
   return (
     <div>
-      hi
+     <Routes>
+        <Route path='/login' element={<UserLogin />} />
+        
+        <Route path='/signup' element={<UserSignup />} />
+        <Route path='/captain-login' element={<Captainlogin />} />
+        <Route path='/captain-signup' element={<CaptainSignup />} />
+      </Routes>
     </div>
   )
 }
