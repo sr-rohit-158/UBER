de2922e7f70dbc327feb6fc1a23a1cfd6ac34b38
sr-rohit-158 <rohit.dhori@swiftrut.com>
2025-01-20 22:41:41 +0530

    add

--- Backend/app.js
+++ Backend/app.js
@@ -12,7 +12,12 @@ const rideRoutes = require('./routes/ride.routes');
 
 connectToDb();
 
-app.use(cors());
+app.use(
+    cors({
+      origin: "http://localhost:5173", 
+      credentials: true, 
+    })
+  );
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
--- Backend/socket.js
+++ Backend/socket.js
@@ -7,7 +7,7 @@ let io;
 function initializeSocket(server) {
     io = socketIo(server, {
         cors: {
-            origin: '*',
+            origin: "http://localhost:5173",
             methods: [ 'GET', 'POST' ]
         }
     });
